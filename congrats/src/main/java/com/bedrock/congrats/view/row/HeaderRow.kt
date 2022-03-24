24ee536affc1f45c69e129c95e2b78bae714a542
Juan Camilo Villada <juan.villada@mercadolibre.com.co>
2022-03-24 16:52:22 -0500

    feat: Add Congrats HeaderRow component (WIP)

--- congrats/src/main/java/com/bedrock/congrats/view/row/HeaderRow.kt
+++ congrats/src/main/java/com/bedrock/congrats/view/row/HeaderRow.kt
@@ -3,17 +3,13 @@ package com.bedrock.congrats.view.row
 import android.content.res.Configuration
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -59,8 +55,23 @@ fun HeaderRow(
                 )
             }
         }
-        Row {
-            Text(text = title, color = Color.White, fontSize = 20.sp, fontWeight = FontWeight.SemiBold)
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween,) {
+            Column {
+                Text(
+                    text = title,
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+
+            Column(modifier = Modifier.wrapContentWidth(), horizontalAlignment = Alignment.End) {
+                Icon(
+                    painter = painterResource(id = icon),
+                    contentDescription = null, // decorative element
+                    tint = Color.White
+                )
+            }
         }
     }
 }
