3fbe9c186da790028235dbfb52e27331b9c5b200
Juan Vilada <jvillad1@gmail.com>
2022-03-28 08:35:54 -0500

    chore: Add new Compose dependencies

--- congrats/src/main/java/com/bedrock/congrats/view/row/HeaderRow.kt
+++ congrats/src/main/java/com/bedrock/congrats/view/row/HeaderRow.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -62,7 +63,9 @@ fun HeaderRow(
                     text = title,
                     color = Color.White,
                     fontSize = 20.sp,
-                    fontWeight = FontWeight.SemiBold
+                    fontWeight = FontWeight.SemiBold,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2
                 )
             }
 
@@ -93,7 +96,7 @@ fun HeaderRowPreview() {
     ComponentsPOCTheme {
         HeaderRow(
             backgroundColor = Green,
-            title = "Congrats pantalla verde",
+            title = "Congrats pantalla verde, Congrats pantalla verde, Congrats pantalla verde, " ,
             label = "",
             icon = R.drawable.congrats_ic_product,
             badge = R.drawable.congrats_ic_badge_check
